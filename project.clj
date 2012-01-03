bec0411aad970d4b999666b0361cd6f5e387d8d8
rod <rod@pu-gh.com>
2012-01-03 21:09:39 +0000

    added clojure-json dependency

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject sockso-repl "0.0.1"
   :description "A REPL for Sockso servers"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.danlarkin/clojure-json "1.2-SNAPSHOT"]])
